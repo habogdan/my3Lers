90dc4b74f4051b794ac70d3ed97ca438493f4a15
Bogdan <hagiu.bogdan97@gmail.com>
2018-12-30 22:34:05 +0200

    Finish 95%

--- main/myMovies.js
+++ main/myMovies.js
@@ -1,11 +1,27 @@
-function addMovies(btn){
-    if (btn.innerHTML=="ADD TO WATCH LIST") {
+function addMovies(btn) {
+    if (btn.innerHTML == "ADD TO WATCH LIST") {
         btn.innerHTML = "ON YOUR WISHLIST";
-        btn.style.backgroundColor = "green";
+        btn.style.backgroundColor = "#cccccc";
+        myContent.movies[btn.value].onWatchList = 1;
     }
     else {
         btn.innerHTML = "ADD TO WATCH LIST";
         btn.style.backgroundColor = "black";
+        myContent.movies[btn.value].onWatchList = 0;
+    }
+}
+
+function checkIfChecked() {
+    let poolAnswer = document.getElementsByName('answer');
+    for ( let i = 0; i < poolAnswer.length; i++) {
+        if(poolAnswer[i].checked) {
+            let req = new XMLHttpRequest();
+            req.open("POST", "#", true);
+            req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            req.send("answer=" + i);
+            alert('Thank you for your vote.');
+            break;
+        }
     }
 }
 
---
+++ main/topThree.js
@@ -0,0 +1,86 @@
+function get(url) {
+    // Return a new promise.
+    return new Promise(function(resolve, reject) {
+        // Do the usual XHR stuff
+        let req = new XMLHttpRequest();
+        req.open('GET', url);
+
+        req.onload = function() {
+            // This is called even on 404 etc
+            // so check the status
+            if (req.status === 200) {
+                // Resolve the promise with the response text
+                resolve(req.response);
+            }
+            else {
+                // Otherwise reject with the status text
+                // which will hopefully be a meaningful error
+                reject(Error(req.statusText));
+            }
+        };
+
+        // Handle network errors
+        req.onerror = function() {
+            reject(Error("Network Error"));
+        };
+
+        // Make the request
+        req.send();
+    });
+}
+
+function getJSON(url) {
+    return get(url).then(JSON.parse);
+}
+
+let myContentPromis = getJSON("JSON.json");
+myContentPromis.then(function(myContent){
+
+    for (let i = 0; i < 3; i++) {
+        let paragraphTag = document.createElement("p");
+        let paragraphContent = document.createTextNode(myContent.movies[i].description);
+        let titleTag = document.createElement("h1");
+        let titleContent = document.createTextNode(myContent.movies[i].title);
+        let dateTag = document.createElement("h2");
+        let dateContent = document.createTextNode(myContent.movies[i].date);
+        let aTag = document.createElement("a");
+        aTag.setAttribute('href',myContent.movies[i].aTag);
+        let img = document.createElement("img");
+        img.setAttribute('src',myContent.movies[i].imgSrc);
+        img.setAttribute('alt',myContent.movies[i].imgAlt);
+
+        let divTagContentText = document.createElement("div");
+        let divTagContentImg = document.createElement("div");
+        let divTagContentAll = document.createElement("div");
+
+        paragraphTag.appendChild(paragraphContent);
+        dateTag.appendChild(dateContent);
+        titleTag.appendChild(titleContent);
+        aTag.appendChild(titleTag);
+
+        divTagContentText.appendChild(aTag);
+        divTagContentText.appendChild(dateTag);
+        divTagContentText.appendChild(paragraphTag);
+
+        divTagContentImg.appendChild(img);
+
+        divTagContentAll.appendChild(divTagContentImg);
+        divTagContentAll.appendChild(divTagContentText);
+
+        divTagContentAll.classList.add("content");
+        divTagContentImg.classList.add("content-img");
+        img.classList.add("content-img-size");
+        divTagContentText.classList.add("content-text");
+        titleTag.classList.add("content-title");
+        dateTag.classList.add("content-date");
+        paragraphTag.classList.add("content-description");
+
+        let element = document.getElementById("topList");
+        element.appendChild(divTagContentAll);
+    }
+
+});
+
+
+
+
