269ecf3561526a05dbadda0ebff9add30ad9d8e4
Bogdan <hagiu.bogdan97@gmail.com>
2019-01-08 23:48:36 +0200

    Finish 100%

--- main/js-files/myMovies.js
+++ main/js-files/myMovies.js
@@ -1,12 +1,116 @@
 function addMovies(btn) {
-    if (btn.innerHTML == "ADD TO WATCH LIST") {
-        btn.innerHTML = "ON YOUR WISHLIST";
-        btn.style.backgroundColor = "#cccccc";
-    }
-    else {
-        btn.innerHTML = "ADD TO WATCH LIST";
-        btn.style.backgroundColor = "black";
-    }
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+        }
+    };
+    xhttp.open("PUT", "../request.php?id=" + btn.id, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    window.location.reload();
+}
+let numberOfMovies = 5;
+function getJSON() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let fabulousJSON = JSON.parse(this.responseText).movies;
+            for (let i = 0; i < numberOfMovies; i++) {
+                if (fabulousJSON[i].onWatchList === 1) {
+                    console.log(i + 1 + "");
+                    document.getElementById(i + 1 + "").style.background = "#cccccc";
+                    document.getElementById(i + 1 + "").innerHTML = "ON YOUR WATCH LIST";
+                }
+                else {
+                    document.getElementById(i + 1 + "").style.background = "black";
+                    document.getElementById(i + 1 + "").innerHTML = "ADD TO WATCH LIST";
+                }
+            }
+        }
+    };
+    xhttp.open("GET", "js-files/JSON.json", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
+
+function getJSON2() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let fabulousJSON = JSON.parse(this.responseText).movies;
+            for (let i = numberOfMovies - 1; i < fabulousJSON.length; i++) {
+                if (fabulousJSON[i].onWatchList === 1) {
+                    console.log(i + 1 + "");
+                    document.getElementById(i + 1 + "").style.background = "#cccccc";
+                    document.getElementById(i + 1 + "").innerHTML = "ON YOUR WATCH LIST";
+                }
+                else {
+                    document.getElementById(i + 1 + "").style.background = "black";
+                    document.getElementById(i + 1 + "").innerHTML = "ADD TO WATCH LIST";
+                }
+            }
+        }
+    };
+    xhttp.open("GET", "js-files/JSON.json", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
+
+function createMyWatchList() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let fabulousJSON = JSON.parse(this.responseText).movies;
+            for (let i = 0; i <= fabulousJSON.length; i++) {
+                if (fabulousJSON[i].onWatchList === 1) {
+                    let paragraphTag = document.createElement("p");
+                    let paragraphContent = document.createTextNode(fabulousJSON[i].description);
+                    let titleTag = document.createElement("h1");
+                    let titleContent = document.createTextNode(fabulousJSON[i].title);
+                    let dateTag = document.createElement("h2");
+                    let dateContent = document.createTextNode(fabulousJSON[i].date);
+                    let aTag = document.createElement("a");
+                    aTag.setAttribute('href', fabulousJSON[i].aTag);
+                    let img = document.createElement("img");
+                    img.setAttribute('src', fabulousJSON[i].imgSrc);
+                    img.setAttribute('alt', fabulousJSON[i].imgAlt);
+
+                    let divTagContentText = document.createElement("div");
+                    let divTagContentImg = document.createElement("div");
+                    let divTagContentAll = document.createElement("div");
+
+                    paragraphTag.appendChild(paragraphContent);
+                    dateTag.appendChild(dateContent);
+                    titleTag.appendChild(titleContent);
+                    aTag.appendChild(titleTag);
+
+                    divTagContentText.appendChild(aTag);
+                    divTagContentText.appendChild(dateTag);
+                    divTagContentText.appendChild(paragraphTag);
+
+                    divTagContentImg.appendChild(img);
+
+                    divTagContentAll.appendChild(divTagContentImg);
+                    divTagContentAll.appendChild(divTagContentText);
+
+                    divTagContentAll.classList.add("content");
+                    divTagContentImg.classList.add("content-img");
+                    img.classList.add("content-img-size");
+                    divTagContentText.classList.add("content-text");
+                    titleTag.classList.add("content-title");
+                    dateTag.classList.add("content-date");
+                    paragraphTag.classList.add("content-description");
+
+                    let element = document.getElementById("my-watch-list");
+                    element.appendChild(divTagContentAll);
+                }
+            }
+
+        }
+    };
+    xhttp.open("GET", "js-files/JSON.json", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
 }
 
 function addTopList() {
@@ -19,7 +123,7 @@ function hideTopList() {
 
 function deleteTopList() {
     let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             let element = document.getElementById("created-class");
             element.innerHTML = '';
@@ -37,12 +141,11 @@ function saveTopList() {
     let thirdItem = document.getElementById("thirdItem").value;
     let topList = firstItem + "\n" + secoundItem + "\n" + thirdItem + "\n";
     let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            //alert(this.responseText);
         }
     };
-    xhttp.open("PUT", "../request.php", true);
+    xhttp.open("POST", "../request.php", true);
     xhttp.setRequestHeader("Content-type", "text/plain");
     xhttp.send(topList);
 
@@ -50,10 +153,10 @@ function saveTopList() {
 
 function createMyTopList() {
     let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             let fullText = this.responseText.split('\n');
-            for (let i = 0; i < fullText.length; i++){
+            for (let i = 0; i < fullText.length; i++) {
                 let paragraphTag = document.createElement("p");
                 let paragraphContent = document.createTextNode(fullText[i]);
                 paragraphTag.appendChild(paragraphContent);
@@ -62,7 +165,7 @@ function createMyTopList() {
                 element.appendChild(paragraphTag);
             }
             console.log(fullText.length);
-            if(fullText.length === 1){
+            if (fullText.length === 1) {
                 document.getElementById("delete-button").style.display = "none";
             }
         }
@@ -74,8 +177,8 @@ function createMyTopList() {
 
 function checkIfChecked() {
     let poolAnswer = document.getElementsByName('answer');
-    for ( let i = 0; i < poolAnswer.length; i++) {
-        if(poolAnswer[i].checked) {
+    for (let i = 0; i < poolAnswer.length; i++) {
+        if (poolAnswer[i].checked) {
             let req = new XMLHttpRequest();
             req.open("POST", "#", true);
             req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
--- main/request.php
+++ main/request.php
@@ -1,10 +1,10 @@
 <?php
 $text = file_get_contents('php://input');
 $request = $_SERVER['REQUEST_METHOD'];
+
 switch($request) {
     case 'GET':
         {
-            //echo 'GET';
             if (file_exists ( 'topList.txt' )){
                 $myfile = fopen("topList.txt", "r");
                 $topList = fread($myfile, filesize ( 'topList.txt' ));
@@ -15,22 +15,39 @@ switch($request) {
         }
     case 'POST':
         {
-            //echo 'POST';
-
-            break;
-        }
-    case 'PUT' :
-        {
-            //echo $text;
             $myfile = fopen("topList.txt", "w");
             fwrite($myfile, $text);
             fclose($myfile);
 
             break;
         }
+    case 'PUT' :
+        {
+                $x = file_get_contents('js-files/JSON.json');
+                $y = json_decode($x);
+                $search_id = $_GET['id'];
+                foreach($y->movies as $movie)
+                {
+                    if($movie->id == $search_id)
+                    {
+                        $old_val = $movie->onWatchList;
+                        echo $old_val;
+                        $new_val = 0;
+                        if($old_val == 1)
+                            $mew_val = 0;
+                        else
+                            $new_val = 1;
+
+                        $movie->onWatchList = $new_val;
+                    }
+                }
+                $y = json_encode($y);
+                file_put_contents('js-files/JSON.json', $y);
+
+            break;
+        }
     case 'DELETE' :
         {
-            //echo 'DELETE';
             unlink("topList.txt");
             break;
         }
